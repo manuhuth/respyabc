ac1d3805e27b0323b9aa44c571fabdada9e90b87
manuhuth <64041311+manuhuth@users.noreply.github.com>
2021-03-05 15:15:33 +0100

    Initialize settings.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages
+from setuptools import setup
+
+p = find_packages()
+
+setup(
+    name="temfpy",
+    version="1.1.4",
+    description=("Python package with test functions for various numerical components"),
+    license="MIT",
+    url="https://github.com/OpenSourceEconomics/temfpy",
+    author="OpenSourceEconomics",
+    author_email="eisenhauer@policy-lab.org",
+    packages=p,
+    zip_safe=False,
+    package_data={"utilities": []},
+    include_package_data=True,
+)
