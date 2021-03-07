8cda07bd01c76033e5027122965181abce71bca4
manuhuth <64041311+manuhuth@users.noreply.github.com>
2021-03-07 18:53:32 +0100

    Test for mean squared distance.

---
+++ respyabc/tests/test_distances.py
@@ -0,0 +1,23 @@
+"""Tests for distance functions."""
+import numpy as np
+
+from respyabc.distances import distance_mean_squared
+
+
+def test_mean_squared_distance_2_times_2():
+    x = {'data' : np.array([[1,2], [3,4]])}
+    y = {'data' : np.array([[5,6], [7,8]])}
+    
+    distance = distance_mean_squared(x,y)
+    
+    np.testing.assert_almost_equal(distance, 16)
+    
+
+def test_mean_squared_distance_2_times_5():
+    x = {'data' : np.array([[1,2,3,4,5], [6,7,8,9,10]])}
+    y = {'data' : np.array([[11,12,13,14,15], [16,17,18,19,20]])}
+    
+    distance = distance_mean_squared(x,y)
+    
+    np.testing.assert_almost_equal(distance, 100)
+    
