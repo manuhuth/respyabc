c3bade09760b0667e3122b40caf75a6fdbaebb97
manuhuth <s6mahuth@uni-bonn.de>
2021-03-19 13:39:17 +0100

    Add test for respyabc.

---
+++ respyabc/tests/test_respyabc.py
@@ -0,0 +1,58 @@
+"""Tests for respyabc function."""
+import respy as rp
+import pyabc
+
+from respyabc.models import model
+from respyabc.distances import distance_mean_squared
+from respyabc.respyabc import respyabc
+
+
+def prepare_test(parameter_true, prior_low, prior_size):
+    """Wrapes all steps to run respyabc for one parameter.
+
+    Parameters
+    ----------
+    parameter_true : dict
+        A dictionary containing the true parameter
+
+    prior_low : float
+        A float with the lower bound for the uniform prior.
+
+    prior_size : float
+        A float containing the length of the uniform prior.
+
+    Returns
+    -------
+    Runs respyabc for the specified parameter.
+    """
+
+    params, options, data_stored = rp.get_example_model("kw_94_one")
+    options["n_periods"] = 40
+    options["simulation_agents"] = 1000
+    model_to_simulate = rp.get_simulate_func(params, options)
+
+    data = model(
+        parameter_true,
+        model_to_simulate=model_to_simulate,
+        parameter_for_simulation=params,
+        options_for_simulation=options,
+    )
+
+    parameters_prior = {"wage_a_constant": [prior_low, prior_size]}
+
+    respyabc(
+        model=model,
+        parameters_prior=parameters_prior,
+        data=data,
+        distance_abc=distance_mean_squared,
+        sampler=pyabc.sampler.MulticoreEvalParallelSampler(),
+        population_size_abc=3,
+        max_nr_populations_abc=2,
+        minimum_epsilon_abc=0.1,
+        database_path_abc=None,
+    )
+
+
+def test_wage_a_constant():
+    parameter_true = {"wage_a_constant": 9.21}
+    prepare_test(parameter_true=parameter_true, prior_low=9, prior_size=0.9)
